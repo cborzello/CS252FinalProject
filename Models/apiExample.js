7b2527234c3cab558082b9e07745a8859845a8cd
Darpan Tanna <dtanna@purdue.edu>
2017-12-06 20:10:12 -0500

    Changed video names to fit dark sky api

--- Models/apiExample.js
+++ Models/apiExample.js
@@ -1,10 +1,29 @@
 var api = require("./api.js");
 
-var state = "California";
-var city = "Los Angelos";
-api.getCoordinates(city, state, function(result) {
-  result=JSON.parse(result);
-  var lattitude = result["results"][0]["geometry"]["location"]["lat"];
-  var longitude = result["results"][0]["geometry"]["location"]["lng"];
-  console.log("Lattitude:" + lattitude + "\nLongitude: " + longitude);
-});
+testDarkSky();
+
+function testGeoLocation() {
+  var state = "California";
+  var city = "Los Angelos";
+  api.getCoordinates(city, state, function(result) {
+    result=JSON.parse(result);
+    var lattitude = result["results"][0]["geometry"]["location"]["lat"];
+    var longitude = result["results"][0]["geometry"]["location"]["lng"];
+    console.log("Lattitude:" + lattitude + "\nLongitude: " + longitude);
+  });
+}
+
+function testDarkSky() {
+  var state = "California";
+  var city = "Los Angelos";
+  api.getCoordinates(city, state, function(result) {
+    result=JSON.parse(result);
+    var lattitude = result["results"][0]["geometry"]["location"]["lat"];
+    var longitude = result["results"][0]["geometry"]["location"]["lng"];
+    console.log("Lattitude:" + lattitude + "\nLongitude: " + longitude);
+    api.getWeather(lattitude, longitude, function(weather){
+      weather= JSON.parse(weather);
+        console.log(weather);
+    });
+  });
+}
